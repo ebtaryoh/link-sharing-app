503f4a0fb6c93ccd8aec81a90d33d069fc2e48e7
Ibitayo Akinnibosun <ibitayo.akinnibosun@gmail.com>
2024-07-25 16:47:49 +0100

    updated commit

--- src/app/components/Navbar/page.tsx
+++ src/app/components/Navbar/page.tsx
@@ -1,17 +1,24 @@
 import Image from "next/image";
 import devlogo from "../../../../public/devlinks.png";
-
-
+import links from "../../../../public/lock.svg";
+import profile from "../../../../public/ph_user-circle-bold.svg";
 
 export default function Navbar() {
-    return(
-        <div>
-             <div className="flex flex-row gap-3 mb-10">
+  return (
+    <div className="">
+      <div className="flex flex-row gap-3 mb-10">
         <Image src={devlogo} alt="devlinks" />
         <p className="font-bold text-2xl">devlinks</p>
       </div>
-        </div>
-    )
-
+      <div className="flex flex-row">
+        <Image src={links} alt="/links" />
+        <p>Links</p>
+      </div>
+      <div>
+        <Image src={profile} alt="profile" />
+        <p>Profile Details</p>
+      </div>
+      <p>Preview</p>
+    </div>
+  );
 }
-
